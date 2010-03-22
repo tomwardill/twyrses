f546d5154fffb3d6e06e01eb6f83b9ae49807aa7
Tom Wardill <tom@howrandom.net>
2010-03-22 16:04:21 +0000

    Can update statuses again

--- twyrses.py
+++ twyrses.py
@@ -499,25 +499,12 @@ class Twyrses(object):
 
     def update_status(self, text):
         """Update that status"""
-        try:
-            api = oauthtwitter.OAuthApi(str(user.screen_name), str(user.password))
-            #api.PostUpdate(text)
+        
+        api = self.get_api()
 
-            if not api._username:
-                raise TwitterError("The twitter.Api instance must be authenticated.")
-            if len(text) > 140:
-                raise TwitterError("Text must be less than or equal to 140 characters.")
-            url = 'http://twitter.com/statuses/update.json'
-            data = {'status': text, 'source': 'twyrses'}
-            json = api._FetchUrl(url, post_data=data)
-            data = simplejson.loads(json)
-            st = Status.NewFromJsonDict(data)			
-
-            self.status_data.insert(0, st)
-            self.draw_timeline()
-        except HTTPError, e:
-            if e.code == 401:
-                self.set_header_text("login to update")
+        if len(text) > 140:
+            raise TwitterError("Text must be less than or equal to 140 characters.")
+        api.update_status(text)
 
     def check_following(self, twittard1, twittard2):
         """Check if one twittard is following another"""
