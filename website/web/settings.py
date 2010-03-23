a1b86e8dc2b0a1f6584e880cb1f24a73e132f7a2
Tom Wardill <tom@howrandom.net>
2010-03-23 16:18:57 +0000

    Some more styling for front page

--- website/web/settings.py
+++ website/web/settings.py
@@ -54,7 +54,7 @@ MEDIA_ROOT = os.path.join(CURRENT_DIR, 'static')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8000/static/'
+MEDIA_URL = 'http://zerogravitas:8000/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
